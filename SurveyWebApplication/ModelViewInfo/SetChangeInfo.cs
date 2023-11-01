53f095bd02af8b533bb04132bb5327a6cfb1de4f
Shoriful Islam Robi <shorifulislam776@gmail.com>
2023-11-01 19:00:10 +0600

    novembar01

---
+++ SurveyWebApplication/ModelViewInfo/SetChangeInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SurveyWebApplication.ModelViewInfo
+{
+    public  class SetChangeInfo
+    {
+        public int SetId { get; set; }
+        public List<RemoveList> datalist { get; set; }
+        public string Connectingstringlist { get; set; }
+    }
+
+
+   
+}
